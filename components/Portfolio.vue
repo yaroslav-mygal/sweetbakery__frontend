323d9663e020b5a5943ac8405098eb82d8b08a34
Ярослав <migal.yaroslav@gmail.com>
2021-04-14 00:44:57 +0300

    FIX

--- components/Portfolio.vue
+++ components/Portfolio.vue
@@ -5,8 +5,19 @@
         <h2 class="section-header__title">Наші роботи</h2>
         <p>Всі фото</p>
       </header>
+      <stack :column-min-width="320" :gutter-width="8" :gutter-height="8">
+        <stack-item>fdfdfd</stack-item>
+        <stack-item>fdfdfd</stack-item>
+        <stack-item>fdfdfd</stack-item>
+        <stack-item>fdfdfd</stack-item>
+        <stack-item>fdfdfd</stack-item>
+        <stack-item>fdfdfd</stack-item>
+        <stack-item>fdfdfd</stack-item>
+      </stack>
+      <br>
+      <a href="#" @click="add">Some button s</a>
+      {{name}},
       {{ message }}
-      <a href="#" v-on:click="calc">Some button</a>
     </div>
   </div>
 </template>
@@ -14,11 +25,22 @@
 export default {
   data: function() {
     return {
-       message: 'my message'
+      name:'my name 1',
+      message: 'my message'
     }
   },
-  calc() {
-    message = 1;
+  mounted: {
+    add: function(event) {
+      event.preventDefault();
+      console.log(this.message)
+      this.message = 1;
+      this.name = 'yarik';
+      console.log(this.message);
+      // message = 1;
+    }
+  },
+  methods: {
+    
   }
 }
 </script>
