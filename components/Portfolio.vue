e6845219fb39d5cb8e70a1e822b415bf397e8009
Ярослав <migal.yaroslav@gmail.com>
2021-04-13 10:24:44 +0300

    my changes

--- components/Portfolio.vue
+++ components/Portfolio.vue
@@ -5,9 +5,23 @@
         <h2 class="section-header__title">Наші роботи</h2>
         <p>Всі фото</p>
       </header>
+      {{ message }}
+      <a href="#" v-on:click="calc">Some button</a>
     </div>
   </div>
 </template>
+<script>
+export default {
+  data: function() {
+    return {
+       message: 'my message'
+    }
+  },
+  calc() {
+    message = 1;
+  }
+}
+</script>
 <style>
   
 </style>
---
+++ plugins/vue-stack-grid.js
@@ -0,0 +1,5 @@
+import Vue from 'vue'
+import { Stack, StackItem } from 'vue-stack-grid';
+// import 'vue-snap/dist/vue-snap.css'
+
+Vue.use(Stack)
