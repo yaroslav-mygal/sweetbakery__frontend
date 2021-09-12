43fd6ffb20952974dfca7a453b28b8effecde9af
Ярослав <migal.yaroslav@gmail.com>
2021-09-12 10:32:57 +0300

    ???

---
+++ components/OrderForm-2.vue
@@ -0,0 +1,160 @@
+<template>
+  <div class="order-content">
+    <div class="order-form">
+      
+        <div class="columns">
+          <div class="column">
+<!-- <b-field label="Select datetime">
+            <b-datetimepicker
+            v-model="order.date"
+                placeholder="Type or select a date..."
+                icon="calendar-today"
+                locale="ru-Ru"
+                editable>
+            </b-datetimepicker>
+        </b-field> -->
+        <input v-model="message" placeholder="отредактируй меня">
+<p>Введённое сообщение: {{ message }}</p>
+          </div>
+          <div class="column">
+            <b-field label="Продукція">
+              <b-select placeholder="Select a name">
+                  <option v-for="(item, index) in formData.type" :key="index">{{ item[getcurrentLang] }}</option>
+              </b-select>
+            </b-field>
+          </div>
+        </div>
+        <!-- torts -->
+        <div class="columns">
+          <div class="column">
+            <b-field label="Вага">
+              <b-select placeholder="Select a name" >
+                  <option v-for="(item, index) in formData.tortData.weight" :key="index">{{ item }}</option>
+              </b-select>
+            </b-field>
+          </div>
+          <div class="column">
+            <label for="">Вага</label>
+            <select v-model="count">
+              <option v-for="(item, index) in formData.tortData.weight" :key="index">{{ item }}</option>
+            </select>
+          </div>
+        </div>
+        
+        <!-- tort -->
+        <!-- <div v-if="order.type === 'Торт'">
+          <div class="form-group" >
+            <label for="">Тип</label>
+            <select v-model="order.cake.category">
+              <option v-for="(item, index) in form.cake.category" :key="index">{{ item }}</option>
+            </select>
+          </div>
+          <div class="form-group" >
+            <label for="">Вага</label>
+            <select v-model="order.cake.weight">
+              <option v-for="(item, index) in form.cake.weight" :key="index">{{ item }}</option>
+            </select>
+          </div>
+        </div> -->
+        <!--  -->
+        
+        <!--  -->
+        <!-- <div class="form-group" v-if="order.type === 'Тістечка'">
+          <label for="">Тип</label>
+          <select v-model="order.tisto.category">
+            <option v-for="(item, index) in form.tisto.category" :key="index">{{ item }}</option>
+          </select>
+        </div> -->
+         <!-- <div class="form-group">
+          <label for="">Кількість</label>
+          <input type="text" v-model="order.amount">
+        </div> -->
+        
+        <div class="form-group">
+          <button @click.prevent="calc">Calc</button>
+        </div>
+    </div>
+    <aside class="order-aside">
+      {{ getcurrentLang }}
+      {{ count }}
+    </aside>
+  </div>
+  
+</template>
+
+<script>
+import Vue from 'vue'
+
+export default Vue.extend({
+  methods: {
+  },
+  computed: {
+    getcurrentLang() {
+      return "ru";
+    },
+  },
+  data() {
+    return {
+      formData: {
+        type: {
+          tort: {
+            uk: 'Торт',
+            ru: 'Торт'
+          },
+          cake: {
+            uk: 'Капкейки',
+            ru: 'Капкейки'
+          },
+          meringue: {
+            uk: 'Меренга',
+            ru: 'Меренга'
+          },
+          cakepops: {
+            uk: 'Кейк-попси',
+            ru: 'Кейк-попсы'
+          },
+          pp: {
+            uk: 'ПП-продукція',
+            ru: 'ПП-продукция'
+          }
+        },
+        tortData: {
+          weight: ['2', '3', '4']
+        },
+      },
+      message: '',
+      count: 1,
+      // order: {
+      //   date: '',
+      //   name: '',
+      //   type: '',
+      //   tort: {
+      //     weight: '',
+      //   },
+      //   amount: 0,
+      //   price: 0,
+      //   cake: {
+      //     category: '',
+      //     weight: 0
+      //   },
+      //   tisto: {
+      //     category: ''
+      //   }
+      // }
+    }
+  },
+})
+</script>
+
+<style scoped>
+  .order-content {
+    display: flex;
+  }
+  .order-form {
+    flex-grow: 1;
+  }
+  .order-aside {
+    flex: 0 0 200px;
+    padding-left: 35px;
+  }
+</style>
--- components/OrderForm.vue
+++ components/OrderForm.vue
@@ -1,59 +1,28 @@
 <template>
   <div class="order-content">
     <div class="order-form">
-      <form action="#">
-        <b-datepicker v-model="date" 
-        inline 
-        :unselectable-days-of-week="[0, 6]">
-      </b-datepicker>
-        <b-field label="Simple">
-            <b-select placeholder="Select a name" v-model="order.type">
-                <option v-for="(item, index) in form.type" :key="index">{{ item }}</option>
-            </b-select>
-        </b-field>
-        <!-- tort -->
-        <div v-if="order.type === 'Торт'">
-          <div class="form-group" >
-            <label for="">Тип</label>
-            <select v-model="order.cake.category">
-              <option v-for="(item, index) in form.cake.category" :key="index">{{ item }}</option>
-            </select>
-          </div>
-          <div class="form-group" >
-            <label for="">Вага</label>
-            <select v-model="order.cake.weight">
-              <option v-for="(item, index) in form.cake.weight" :key="index">{{ item }}</option>
-            </select>
-          </div>
-        </div>
-        <!--  -->
-        <div v-if="order.type === 'Капкейк'">
-          <div class="form-group" >
+      
+        <!-- torts -->
+        <div class="columns">
+          <div class="column">
             
           </div>
-        </div>
-        <!--  -->
-        <div class="form-group" v-if="order.type === 'Тістечка'">
-          <label for="">Тип</label>
-          <select v-model="order.tisto.category">
-            <option v-for="(item, index) in form.tisto.category" :key="index">{{ item }}</option>
-          </select>
-        </div>
-         <div class="form-group">
-          <label for="">Кількість</label>
-          <input type="text" v-model="order.amount">
-        </div>
-        <div class="form-group">
-          <label for="">Дата</label>
-          <input type="text" v-model="order.date">
+          <div class="column">
+            <label for="">Вага</label>
+            <select v-model="count">
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+            </select>
+          </div>
         </div>
         <div class="form-group">
           <button @click.prevent="calc">Calc</button>
         </div>
-      </form>
     </div>
     <aside class="order-aside">
-      {{ order.type }}
+      Message: {{ message }}
+      <p>{{ count }}</p>
     </aside>
   </div>
   
@@ -62,48 +31,17 @@
 <script lang="ts">
 import Vue from 'vue'
 
-export default Vue.extend({
+export default Vue.extend ({
   data() {
     return {
-      form: {
-        type: ['Торт', 'Капкейк', 'Тістечка', 'Меренга', 'Кейк-попси', 'ПП-продукція'],
-        cake: {
-          category: ['Бісквітний', 'Мусовий', 'Класика'],
-          weight: ['1', '1.5', '2', '2.5']
-        },
-        cupcake: {
-          price: 50
-        },
-        tisto: {
-          category: ['Ескімо', 'Мусові', 'Відкриті']
-        },
-        meringue: {
-
-        },
-        cakepops: {
-
-        }
-      },
-      order: {
-        name: '',
-        type: 'Торт',
-        date: '12.06.2021',
-        amount: 0,
-        price: 0,
-        cake: {
-          category: '',
-          weight: 0
-        },
-        tisto: {
-          category: ''
-        }
-      }
+      count : 1,
+      message: 'This is a message'
     }
   },
   methods: {
     calc() {
-      console.log('calc');
-      console.log(this.order);
+      this.message = `other`;
+      ++this.count;
     }
   },
 })
